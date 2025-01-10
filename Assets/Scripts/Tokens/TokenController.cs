a73b2f46359e0cf7c363c2fe8195abdbcadfee4b
Zzalbs <57441250+ZZalbs@users.noreply.github.com>
2025-01-11 00:22:13 +0900

    토큰컨트롤러 삭제 함수 추가

--- Assets/Scripts/Tokens/TokenController.cs
+++ Assets/Scripts/Tokens/TokenController.cs
@@ -7,13 +7,33 @@ public class TokenController : MonoBehaviour
     public BehaviourController PlayerBehaviourController;
     public LifeController LifeController;
     public InputHandler InputController;
-
-    public List<TokenBase> tokens;
+    public List<TokenBase> PositiveTokens;
+    public List<TokenBase> NegativeTokens;
 
     private void Awake()
     {
         PlayerBehaviourController = GetComponent<BehaviourController>();
         LifeController = GetComponent<LifeController>();
         InputController = GetComponent<InputHandler>();
+    }
+
+
+    public void DestroyOnePositiveToken(int index)
+    {
+        PositiveTokens.RemoveAt(index);
+    }
+    public void DestroyOneNegativeToken(int index)
+    {
+        NegativeTokens.RemoveAt(index);
+    }
+
+    public void DestroyAllPositiveToken()
+    {
+        PositiveTokens.Clear();
+    }
+
+    public void DestroyAllNegativeToken()
+    {
+        NegativeTokens.Clear();
     }
 }
