562ceb8fc2e334fa6d73aeef78dc7534cdcbafe4
Iosif2510 <gpotatok@gmail.com>
2025-01-11 10:45:47 +0900

    InvincibleToken 완성

--- Assets/Scripts/Tokens/InvincibleToken.cs
+++ Assets/Scripts/Tokens/InvincibleToken.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InvincibleToken : MonoBehaviour
+public class InvincibleToken : TokenBase
 {
-    // Start is called before the first frame update
-    void Start()
+    private float invincibleTime => 3 + 0.5f * (CurLevel - 1);
+    private float timeCounter = 0f;
+
+    private LifeController playerLife;
+
+    public InvincibleToken(TokenController controller, string name, string description, Rarity rarity, bool isPositive, int maxLevel) : base(controller, name, description, rarity, isPositive, maxLevel)
     {
-        
+        playerLife = controller.LifeController;
+        timeCounter = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override float Timer => timeCounter / invincibleTime;
+    
+    public override void OnStartStage()
     {
-        
+        timeCounter = 0f;
+        playerLife.SetImmuneForTime(invincibleTime);
+    }
+
+    public override void Update()
+    {
+        if (timeCounter < invincibleTime)
+        {
+            timeCounter += Time.deltaTime;
+            if (timeCounter >= invincibleTime) timeCounter = invincibleTime;
+        }
+
     }
 }
