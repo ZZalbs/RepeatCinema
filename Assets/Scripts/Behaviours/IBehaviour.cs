1e08f49256485d9a7b718457b5e91a3e6a70d3a3
homebd <118789343+homebd@users.noreply.github.com>
2025-01-11 19:21:15 +0900

    Revert "Feat monster"

--- Assets/Scripts/Behaviours/IBehaviour.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine.InputSystem;
-
-public interface IBehaviour
-{
-    public BehaviourType Type { get; }
-    public void OnPressed(InputAction.CallbackContext ctx);
-    public void OnReleased(InputAction.CallbackContext ctx);
-}
