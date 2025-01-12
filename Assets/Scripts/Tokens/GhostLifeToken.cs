4114cd2937e307548788b8c37970310f4da11ed5
homebd <118789343+homebd@users.noreply.github.com>
2025-01-13 02:48:18 +0900

    유령 체력 버그 수정하여 추가
    onHit += 으로 추가했어서 그런 듯
    onHIt = 으로 해도 됐을 거 같은데 그냥 매서드로 만들어서 수정

--- Assets/Scripts/Tokens/GhostLifeToken.cs
+++ Assets/Scripts/Tokens/GhostLifeToken.cs
@@ -1,12 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.Events;
 
 public class GhostLifeToken : TokenBase
 {
-    private UnityAction onHit;
-    
     public GhostLifeToken(TokenController controller, string name, string description, Rarity rarity, bool isPositive, int maxLevel) : base(controller, name, description, rarity, isPositive, maxLevel)
     {
     }
@@ -18,20 +13,21 @@ public class GhostLifeToken : TokenBase
     public override void Acquire()
     {
         base.Acquire();
-        var controllerLifeController = controller.LifeController;
-        onHit += () =>
-        {
-            controllerLifeController.CurLife++;
-            if (CurLevel > 2) CurLevel--;
-            else controller.DestroyToken(this);
-        };
-        
-        controllerLifeController.onHit += onHit;
+
+        controller.LifeController.onHit += onHit;
     }
     
     public override void OnDestroy()
     {
         base.OnDestroy();
+
         controller.LifeController.onHit -= onHit;
+    }
+
+    private void OnHit()
+    {
+        controller.LifeController.CurLife++;
+        if (CurLevel > 2) CurLevel--;
+        else controller.DestroyToken(this);
     }
 }
