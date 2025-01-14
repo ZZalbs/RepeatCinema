9e37bb457eba62aa1de8b989e2991366d0ddbdf2
homebd <118789343+homebd@users.noreply.github.com>
2025-01-14 13:09:20 +0900

    튜토리얼 볼 때 야매 이동 잠그기

--- Assets/Scripts/UIs/TutorialUI.cs
+++ Assets/Scripts/UIs/TutorialUI.cs
@@ -6,7 +6,7 @@ public class TutorialUI : MonoBehaviour
     private PlayerSettingDataSO playerSetting;
 
     [SerializeField] private Toggle tutorialSkip;
-
+    [SerializeField] private BehaviourController player;
     private void Start()
     {
         playerSetting = Resources.Load<PlayerSettingDataSO>("PlayerSettingData");
@@ -17,5 +17,15 @@ public class TutorialUI : MonoBehaviour
     public void ToggleTutorialSkip()
     {
         playerSetting.NeedTutorial = !tutorialSkip.isOn;
+    }
+
+    private void Update()
+    {
+        player.SetMovable(false);
+    }
+
+    private void OnDisable()
+    {
+        player.SetMovable(true);
     }
 }
